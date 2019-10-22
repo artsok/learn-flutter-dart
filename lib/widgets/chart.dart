dc32a05abb07d969cf8baa154b67e62c502d1a2c
artsok <relix@mail.ru>
2019-10-22 17:45:06 +0300

    done up 101. Finishing the Chart Bars

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -41,16 +41,23 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        //Требует сгенерить показатели для каждого дня
-        children: groupedTransactionValues.map((data) {
-          //return Text("${data['day']} : ${data['amount']}");
-          return CharBar(
-            data["day"],
-            data["amount"],
-            maxSpending == 0.0 ? 0.0: (data['amount'] as double) / maxSpending,
-          );
-        }).toList(),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //Требует сгенерить показатели для каждого дня
+          children: groupedTransactionValues.map((data) {
+            //return Text("${data['day']} : ${data['amount']}");
+            return Flexible(
+              fit: FlexFit.tight, //Запихиваем child в размеры CharBar, не занимаем другое простанство child!
+              child: CharBar(
+                data["day"],
+                data["amount"],
+                maxSpending == 0.0 ? 0.0: (data['amount'] as double) / maxSpending,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
